e4bde9e94775bc3aa9b6345124b743f447b2d69b
Carlos-Santoss <carlos.santos@globalred.com.br>
2024-04-08 12:15:06 -0300

    removed Column annotation

--- src/main/java/in/bushansirgur/springrestapi/model/Employee.java
+++ src/main/java/in/bushansirgur/springrestapi/model/Employee.java
@@ -6,6 +6,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -13,22 +17,31 @@ import lombok.ToString;
 @Entity
 @Table(name = "tbl_employee")
 public class Employee {
+    /*
+    * Foi visto que nao é necessario
+    * adicionar a annotation @Column
+    * quando se está utilizando JPA
+    * */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id" )
+    //@Column(name = "id" )
     private Long id;
-
     //@JsonProperty("full_name") como o atributo será mostrado no Json de resposta
-    @Column(name = "name" )
+    //@Column(name = "name" )
     private String name;
     //@JsonIgnore Será ignorado no payload de resposta
-    @Column(name = "age" )
+    //@Column(name = "age" )
     private Long age;
-    @Column(name = "location" )
+    //@Column(name = "location" )
     private String location;
-    @Column(name = "email" )
+    //@Column(name = "email" )
     private String email;
-    @Column(name = "department" )
+    //@Column(name = "department" )
     private String department;
-
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)// esse aqui vamos manter pois queremos o valor diferente no bd e no atributo
+    private Date createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")// esse aqui vamos manter pois queremos o valor diferente no bd e no atributo
+    private Date updatedAt;
 }
